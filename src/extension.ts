49f107c3787d52ccafcc5e33c6b059d43afecc94
XxA7med66xX <XxAhmed66xX@proton.me>
2024-06-07 08:31:42 +0300

    Initial commit

---
+++ src/extension.ts
@@ -0,0 +1,55 @@
+import * as vscode from 'vscode';
+import { getPermissions } from './permissions';
+
+export function activate(context: vscode.ExtensionContext) {
+
+	const disposable = vscode.commands.registerCommand('Permission.add', () => {
+		showPermissionsMenu();
+	});
+
+	const codeActionProvider = vscode.languages.registerCodeActionsProvider('xml', new XmlCodeActionProvider(), {
+		providedCodeActionKinds: XmlCodeActionProvider.providedCodeActionKinds
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+function showPermissionsMenu() {
+
+	const quickPick = vscode.window.createQuickPick();
+	quickPick.items = getPermissions;
+	quickPick.placeholder = 'Select permission to add';
+	quickPick.matchOnDetail = true;
+	quickPick.matchOnDescription = true;
+
+	quickPick.onDidChangeSelection(item => {
+
+		const selection = `<uses-permission android:name="android.permission.${item[0].label}"/>`
+		const editor = vscode.window.activeTextEditor;
+
+		if (editor) {
+			editor.edit(editBuilder => {
+				editBuilder.insert(editor.selection.active, selection);
+			});
+		}
+
+		quickPick.dispose()
+
+	});
+
+	quickPick.onDidHide(() => {
+		quickPick.dispose();
+	});
+
+	quickPick.show();
+}
+
+class XmlCodeActionProvider implements vscode.CodeActionProvider {
+	public static readonly providedCodeActionKinds = [vscode.CodeActionKind.QuickFix];
+
+	provideCodeActions(document: vscode.TextDocument, range: vscode.Range, context: vscode.CodeActionContext, token: vscode.CancellationToken): vscode.CodeAction[] {
+		const fix = new vscode.CodeAction('Add permission', vscode.CodeActionKind.QuickFix);
+		fix.command = { command: 'Permission.add', title: 'Add permission', arguments: [document.uri, range] };
+		return [fix];
+	}
+}
