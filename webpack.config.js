3156258c84cdf271dbb3fd14345ed580bf3e98ce
XxA7med66xX <XxAhmed66xX@proton.me>
2025-03-29 17:51:34 +0300

    feat: Bundle the extension using webpack
    This will significantly reduce the extension
    file size from 17MB to 430KB.

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/extension.ts',
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'out'),
+    filename: 'extension.js',
+    libraryTarget: 'commonjs2'
+  },
+  externals: {
+    vscode: 'commonjs vscode'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: 'ts-loader'
+      }
+    ]
+  }
+};
