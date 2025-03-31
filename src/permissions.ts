8041e82b737f0435d65319621e8f5b07f0b6d978
XxA7med66xX <XxAhmed66xX@proton.me>
2025-03-31 19:45:42 +0300

    feat: List newly added permissions in each build
    When building the extension, it fetches the permissions
    from the android website and compares the fetched permissions
    with the cached ones. If there are any newly added permissions
    it prints them in the console.

--- src/permissions.ts
+++ src/permissions.ts
@@ -5,17 +5,17 @@ import * as cheerio from 'cheerio';
 /// Android permissions source.
 const URL: string = 'https://developer.android.com/reference/kotlin/android/Manifest.permission';
 
+/// Cached permissions path.
+const cacheFilePath: string = path.join('asset', 'permissions.json');
+
 export async function getPermissions(extensionPath: string): Promise<{ label: string; code: string; }[]> {
-    const jsonFilePath = path.join(extensionPath, 'asset', 'permissions.json');
-    const data = await fs.readFile(jsonFilePath, 'utf8');
+    const data = await readCachedPermissions(extensionPath);
     return JSON.parse(data).map((e: String) => {
         return { label: e, code: `<uses-permission android:name="android.permission.${e}"/>` };
     });
 }
 
 export async function fetchPermissions() {
-  const cacheFilePath = path.join('asset', 'permissions.json');
-
   try {
     const response = await fetch(URL);
     const body = await response.text();
@@ -33,6 +33,7 @@ export async function fetchPermissions() {
     });
 
     const permissionsArray = Array.from(allPermissions).sort();
+    await compareDiff(permissionsArray);
 
     await fs.writeFile(cacheFilePath, JSON.stringify(permissionsArray, null, 2));
   } catch (error) {
@@ -40,3 +41,37 @@ export async function fetchPermissions() {
   }
 }
 
+/**
+ * Pass the extension path only if you will read cached
+ * permissions inside the installed extension; 
+ * otherwise, leave it empty.
+*/
+async function readCachedPermissions(extensionPath: string = ''): Promise<string> {
+  const jsonFilePath = path.join(extensionPath, 'asset', 'permissions.json');
+  return await fs.readFile(jsonFilePath, 'utf8');
+}
+
+/** Compares fetched permissions with cached ones
+ * and prints any differences. 
+*/
+async function compareDiff(permissions: Array<string>): Promise<void> {
+  if (!(await isFileExists(cacheFilePath))) { return; }
+
+  const cachedPermissions = await readCachedPermissions();
+  const diff = permissions.filter(permission => !cachedPermissions.includes(permission));
+
+  if (diff.length > 0) {
+    console.log('Newly added permissions:', diff, '\n');
+  } else {
+    console.log('No new permissions\n');
+  }
+}
+
+async function isFileExists(path: string): Promise<boolean> {
+  try {
+    await fs.access(path);
+    return true;
+  } catch {
+    return false;
+  }
+}
